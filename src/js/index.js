f1b470da9c008d1b36fbca91d0cb983ad21f5703
PasyakinAlexander <sashapasyakin@yandex.ru>
2021-10-26 00:55:41 +0300

    Адаптив

--- src/js/index.js
+++ src/js/index.js
@@ -5,6 +5,8 @@ function Core()
     SetTabSwitcher();
     SetModal();
     InitOwlCarousel();
+    ShowMenu();
+    CloseMenu();
 }
 
 function InitOwlCarousel()
@@ -52,6 +54,25 @@ function InitOwlCarousel()
     $('.btns').on('click', 'li', function(e) {
         carousel_recommendations.trigger('to.owl.carousel', [$(this).index(), 300]);
     });
+}
+
+function ShowMenu()
+{
+    $('.btn__menu').click(function(){
+        if ($(".nav").hasClass('showMenu')){
+            $(".nav").removeClass('showMenu');
+        }
+        else{
+            $(".nav").addClass('showMenu');
+        }
+    })
+}
+
+function CloseMenu()
+{
+    $('.btnCloseMenu').click(function(){
+        $(".nav").removeClass('showMenu');
+    })
 }
 
 function SetTabSwitcher()
